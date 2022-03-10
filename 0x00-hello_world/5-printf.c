2c94045475b96f6ded70081109043d9380c747b2
Zeepythonister <zeepythonister@gmail.com>
2022-03-10 22:31:42 +0000

    script that executes C file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- *  main - Entry point
- *  Description: prints a string
- *  Return : Always 0 (Success)
- */
+* main - prints string
+* Return : Always 0 (Success)
+**/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
 }

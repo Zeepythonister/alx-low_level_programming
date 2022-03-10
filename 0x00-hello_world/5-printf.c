c6638e5f2a20670db7dee6e18aed68f8837430ce
Zeepythonister <zeepythonister@gmail.com>
2022-03-10 23:58:45 +0000

    fix stderr

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,10 @@
 * Return: Always 0 (Success)
 *
 * Description: Main returns string
+* stderr - anything
 */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
-	print STDERR
 }

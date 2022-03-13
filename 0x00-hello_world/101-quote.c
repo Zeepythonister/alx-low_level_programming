7e17b7cc6a004ff55f2c278486a25b9254183685
root <zeepythonister@gmail.com>
2022-03-13 13:05:12 -0700

    fix trailing whitespace

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,12 +7,9 @@
  * Description: and that piece of art is useful-Dora Korpar, 2015-10-19
  * 
  * Return : Always 0 (Success)
- *
- * Description : Main returns string
  */
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-

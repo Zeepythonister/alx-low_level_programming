433eb0608e70734fb487f54762b7e75c3d2d495c
Zeepythonister <zeepythonister@gmail.com>
2022-03-15 19:38:55 -0700

    solution to task 0x02 functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,18 @@
 #include "main.h"
+/* more headers goes there */
 
 /**
- * main - Entry point
+ * main - Program entry point
  *
- * Description: Program that prints _putchar
- *
- * Return: Always 0 (success). Error message otherwise
+ * Return: 0 on success. Error code otherwise
  */
 int main(void)
 {
-  /*declare local variable*/
-	char *a = "_putchar";
-	int a;
+	char ch[8] = {"_putchar"};
+	int i;
 
-	for (a = 0; a < 8; a++)
-		_putchar(*a);
+	for (i = 0; i < 8; i++)
+		_putchar(ch[i]);
 	_putchar('\n');
 	return (0);
 }

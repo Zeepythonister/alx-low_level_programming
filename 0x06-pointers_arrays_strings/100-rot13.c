1421e2ff887f7ccd7fa6f64b6c6e787e25d02db3
Zeepythonister <zeepythonister@gmail.com>
2022-03-24 20:14:51 -0700

    solution to advance tasks

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,28 @@
 #include "main.h"
-
 /**
- * rot13 - encodes a string using rot13.
- * @s: the string
- *
- * Return: the encoded string
+ * rot13 - encodes a string using rot13
+ * @s: input string.
+ * Return: the pointer to dest.
  */
+
 char *rot13(char *s)
 {
-	int i, j;
-	char *letters1 = {"ABCDEFGHIJKLMNOPQRSTUVwXYZabcdefghijklmnopqrstuvwxyz"};
-	char *letters2 = {"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"};
+	int count = 0, i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*(s + count) != '\0')
 	{
-		for (j = 0; j < 52; j++)
+		for (i = 0; i < 52; i++)
 		{
-			if (s[i] == letters1[j])
+			if (*(s + count) == alphabet[i])
 			{
-				s[i] = letters2[j];
+				*(s + count) = rot13[i];
 				break;
 			}
 		}
+		count++;
 	}
+
 	return (s);
 }
